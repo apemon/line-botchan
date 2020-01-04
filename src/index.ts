80b907c2c2b3e4f4be99f18c000c4462b54f5f81
Parin Chiamananthapong <apemon642002@hotmail.com>
2020-01-04 18:13:24 +0700

    initial bot

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,36 @@ import 'dotenv/config'
 
 import express, {Request, Response} from 'express'
 import bodyParser from 'body-parser'
+import { lineMiddleware } from './line'
+import { webhookHandler } from './webhook'
+import { errorHandler } from './middleware/error-handler'
 
 const {PORT = 8000} = process.env
 
-const app = express()
+function main() {
+  const app = express()
 
-app.use(bodyParser())
+  // Setup LINE webhook endpoint.
+  app.post('/webhook', lineMiddleware, webhookHandler)
 
-app.get('/', (req: Request, res: Response) => {
-  res.send({status: 'OK'})
-})
+  // Setup body-parser to parse URL encoded and JSON inputs.
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(bodyParser.json())
+  
+  app.get('/', (_req: Request, res: Response) => {
+    res.send({status: 'OK'})
+  })
 
-app.listen(PORT, () => {
-  console.log(`Server started at 0.0.0.0:${PORT}`)
-})
+  // Handle error responses here.
+  app.use(errorHandler)
+  
+  app.listen(PORT, () => {
+    console.log(`Server started at 0.0.0.0:${PORT}`)
+  })
+}
+
+try {
+  main()
+} catch (error) {
+  console.error('Fatal Error:', error.message)
+}
---
+++ src/webhook.ts
@@ -0,0 +1,33 @@
+import {Request, Response} from 'express'
+import {WebhookRequestBody} from '@line/bot-sdk'
+
+import {client} from './line'
+
+export async function webhookHandler(req: Request, _res: Response) {
+  try {
+    const {events} = req.body as WebhookRequestBody
+
+    console.debug('Events Count:', events.length)
+
+    for (let event of events) {
+      const {userId} = event.source
+      if (!userId) continue
+
+      console.debug('>', event)
+
+      if (event.type !== 'message') continue
+      if (event.message.type !== 'text') continue
+
+      const {text} = event.message
+      console.log('text:', text)
+
+      await client.pushMessage(userId, {
+        type: 'text',
+        text: text
+      })
+      
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
+}
